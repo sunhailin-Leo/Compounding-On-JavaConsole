2d14b402963ed2d161e74d8f701e46d0b5d7e919
sunhailin <379978424@qq.com>
2016-03-11 09:22:01 +0800

    This is the upgrade version.

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,13 @@
+package Calulation;
+
 import java.util.Scanner;
 
 public class Calculate {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("请选择单利计算或复利计算:(复利选1，单利选2)");
+		int choice = scanner.nextInt();
 		System.out.println("请输入本金:");
 		double money = scanner.nextDouble(); // 本金
 		System.out.println("请输入存款年数:");
@@ -10,10 +15,42 @@ public class Calculate {
 		double sum = 0;// 存款总额
 		System.out.println("请输入年利率:");
 		double rate = scanner.nextDouble(); // 利率
-
-		for (int i = 1; i <= years; i++) {
-			sum = (1 + rate) * i * money;
-			System.out.println("存入第" + i + "年后的存款总额为：" + sum);
+		switch(choice)
+		{
+			case 1:
+				CompoundingCalculation(money, years, rate);
+				break;
+			case 2:
+				SimpleInterestCalculation(money, years, rate);
+				break;
+			default:
+				System.out.println("输入错误!");
+				main(null);
+				break;
 		}
+
 	}
+
+		public static void CompoundingCalculation(double money, int years, double rate) 
+		{
+			double sum = 0 ;
+			for (int i = 1; i <= years; i++) 
+			{
+				sum = money*Math.pow(1.0 + rate, years);	
+			}
+			System.out.println("存入第" + years + "年后的存款总额为：" + sum);
+			main(null);
+		}
+		
+		public static void SimpleInterestCalculation(double money, int years, double rate)
+		{
+			double sum;
+			double interest;
+			interest = money*rate*years;
+			sum = money+interest;
+			System.out.println("存入第" + years + "年后的存款总额为：" + sum);
+			main(null);
+		}
+		
+		
 }
